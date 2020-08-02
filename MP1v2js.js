c27282c23fa52dddd69b312c5ad0744ba6c7c93d
camillecay <68919183+camillecay@users.noreply.github.com>
2020-08-03 01:07:09 +0800

    before editing icon links

--- MP1v2js.js
+++ MP1v2js.js
@@ -1,8 +1,6 @@
 
-    //AUTHENTICATION
-    // var email = "camille_cay@dlsu.edu.ph";
-    // var password = "abcd1234";
-   
+
+//------------------------------ AUTHORIZATION --------------------------------------------
 
 function login(){
 
@@ -41,25 +39,15 @@ function hideContent(){
     document.getElementById("letterC").style.display = 'none';
 }
 
-    // Net Ninja
-    //login = function
 
-
-    //user info kuha
-    
-
-
-
+//----------------------------------------------END OF AUTHORIZATION --------------------------------------------
 // --------------------------------------------- GETTING DATA --------------------------------------------------
 
     let educContent = document.querySelector("#educContent")
     function renderEduc(){
-        //getting data netninja
-
         document.getElementById("educContent").innerHTML = '';
             db.collection('educations').get().then(snapshot => {
                 snapshot.docs.forEach(doc => {
-                    // renderEduc(doc);
                     let educ_div = document.createElement('div');
                     educ_div.classList.add("educData");
                     educ_div.setAttribute('id', doc.id);
@@ -93,16 +81,14 @@ function hideContent(){
 
                 educContent.appendChild(educ_div);
 
-                delButton.addEventListener('click', (event) => {
-                    event.stopPropagation();
-                    let getid = event.target.parentElement.getAttribute('id');
-                    db.collection('educations').doc(getid).delete();
-                    renderEduc();
-                })
-
+                    delButton.addEventListener('click', (event) => {
+                        event.stopPropagation();
+                        let getid = event.target.parentElement.getAttribute('id');
+                        db.collection('educations').doc(getid).delete();
+                        renderEduc();
+                    })
                 })
             })
-        
     }
 
     renderEduc();
@@ -110,142 +96,149 @@ function hideContent(){
 
 
     //get org content
-    let orgContent = document.querySelector("#orgContent")
-    function renderOrg(doc){
-        let org_div = document.createElement('div');
-            org_div.classList.add("orgData");
-            org_div.setAttribute('id', doc.id);
+    let orgContent = document.querySelector("#orgContent");
+    function renderOrg(){
 
-        let Oname = document.createElement('div');
-        Oname.classList.add("Oname");
-        Oname.textContent = doc.data().name;
+        document.getElementById("orgContent").innerHTML = '';
+        db.collection('organizations').get().then(snapshot => {
+            snapshot.docs.forEach(doc => {
+            let org_div = document.createElement('div');
+                org_div.classList.add("orgData");
+                org_div.setAttribute('id', doc.id);
+    
+            let Oname = document.createElement('div');
+            Oname.classList.add("Oname");
+            Oname.textContent = doc.data().name;
+    
+            let Oposition = document.createElement('div');
+            Oposition.classList.add("Oposition");
+            Oposition.textContent = doc.data().position;
+    
+            let Oyear_start = document.createElement('div');
+            Oyear_start.classList.add("Oyear_start");
+            Oyear_start.textContent = doc.data().year_start;
+    
+            let Oyear_end = document.createElement('div');
+            Oyear_end.classList.add("Oyear_end");
+            Oyear_end.textContent = doc.data().year_end;
+    
+            let delButton = document.createElement('div');
+                delButton.classList.add("delButton");
+                delButton.textContent= 'x';
+          
+    
+                org_div.appendChild(Oname);
+                org_div.appendChild(Oposition);       
+                org_div.appendChild(Oyear_start);
+                org_div.appendChild(Oyear_end);
+                org_div.appendChild(delButton);
+    
+            orgContent.appendChild(org_div);
+    
+            delButton.addEventListener('click', (event) => {
+                event.stopPropagation();
+                let getid = event.target.parentElement.getAttribute('id');
+                db.collection('organizations').doc(getid).delete();
+                renderOrg();
+            })
 
-        let Oposition = document.createElement('div');
-        Oposition.classList.add("Oposition");
-        Oposition.textContent = doc.data().position;
-
-        let Oyear_start = document.createElement('div');
-        Oyear_start.classList.add("Oyear_start");
-        Oyear_start.textContent = doc.data().year_start;
-
-        let Oyear_end = document.createElement('div');
-        Oyear_end.classList.add("Oyear_end");
-        Oyear_end.textContent = doc.data().year_end;
-
-        let delButton = document.createElement('div');
-            delButton.classList.add("delButton");
-            delButton.textContent= 'x';
-      
-
-            org_div.appendChild(Oname);
-            org_div.appendChild(Oposition);       
-            org_div.appendChild(Oyear_start);
-            org_div.appendChild(Oyear_end);
-            org_div.appendChild(delButton);
-
-        orgContent.appendChild(org_div);
-
-        delButton.addEventListener('click', (event) => {
-            event.stopPropagation();
-            let getid = event.target.parentElement.getAttribute('id');
-            db.collection('organizations').doc(getid).delete();
+            })
         })
-
-
     }
 
-    //getting data netninja
-    db.collection('organizations').get().then(snapshot => {
-        snapshot.docs.forEach(doc => {
-            renderOrg(doc);
-        })
-    })
-
+    renderOrg();
 
     // get data works
 
     let workContent = document.querySelector("#workContent")
-    function renderWork(doc){
-        let work_div = document.createElement('div');
-            work_div.classList.add("workData");
-            work_div.setAttribute('id', doc.id);
+    function renderWork(){
 
-        let Wname = document.createElement('div');
-            Wname.classList.add("Wname");
-            Wname.textContent = doc.data().name;
-
-        let Wlab = document.createElement('div');
-            Wlab.classList.add("Wlab");
-            Wlab.textContent = doc.data().lab;
-
-
-        let Wyear_start = document.createElement('div');
-            Wyear_start.classList.add("Wyear_start");
-            Wyear_start.textContent = doc.data().year_start;
-
-        let Wyear_end = document.createElement('div');
-            Wyear_end.classList.add("Wyear_end");
-            Wyear_end.textContent = doc.data().year_end;
-
-        let delButton = document.createElement('div');
-            delButton.classList.add("delButton");
-            delButton.textContent= 'x';
-      
-
-            work_div.appendChild(Wname);
-            work_div.appendChild(Wlab);       
-            work_div.appendChild(Wyear_start);
-            work_div.appendChild(Wyear_end);
-            work_div.appendChild(delButton);
-
-        workContent.appendChild(work_div);
-
-        delButton.addEventListener('click', (event) => {
-            event.stopPropagation();
-            let getid = event.target.parentElement.getAttribute('id');
-            db.collection('works').doc(getid).delete();
-        })
-
-
-    }
-
-    //getting data netninja
-    db.collection('works').get().then(snapshot => {
-        snapshot.docs.forEach(doc => {
-            renderWork(doc);
-        })
-    })
-//-----------------------------end of work data-----------------------------------
-//----------------------start of others data---------------------------------
-
-
-let introContent = document.querySelector("#introContent")
-function renderIntro(doc){
-    let intro_div = document.createElement('div');
-    intro_div.classList.add("introData");
-    intro_div.setAttribute('id', doc.id);
-
-    let intro = document.createElement('div');
-        intro.classList.add("intro");
-        intro.textContent = doc.data().fact;
-
-
-
-        intro_div.appendChild(intro);
-
-
-        introContent.appendChild(intro_div);
-
-
-
-}
-//getting data netninja
-        db.collection('introductions').get().then(snapshot => {
+        document.getElementById("workContent").innerHTML ='';
+        db.collection('works').get().then(snapshot => {
             snapshot.docs.forEach(doc => {
-                renderIntro(doc);
+                // renderWork(doc);
+
+                let work_div = document.createElement('div');
+                work_div.classList.add("workData");
+                work_div.setAttribute('id', doc.id);
+    
+            let Wname = document.createElement('div');
+                Wname.classList.add("Wname");
+                Wname.textContent = doc.data().name;
+    
+            let Wlab = document.createElement('div');
+                Wlab.classList.add("Wlab");
+                Wlab.textContent = doc.data().lab;
+    
+    
+            let Wyear_start = document.createElement('div');
+                Wyear_start.classList.add("Wyear_start");
+                Wyear_start.textContent = doc.data().year_start;
+    
+            let Wyear_end = document.createElement('div');
+                Wyear_end.classList.add("Wyear_end");
+                Wyear_end.textContent = doc.data().year_end;
+    
+            let delButton = document.createElement('div');
+                delButton.classList.add("delButton");
+                delButton.textContent= 'x';
+          
+    
+                work_div.appendChild(Wname);
+                work_div.appendChild(Wlab);       
+                work_div.appendChild(Wyear_start);
+                work_div.appendChild(Wyear_end);
+                work_div.appendChild(delButton);
+    
+            workContent.appendChild(work_div);
+    
+            delButton.addEventListener('click', (event) => {
+                event.stopPropagation();
+                let getid = event.target.parentElement.getAttribute('id');
+                db.collection('works').doc(getid).delete();
+                renderWork();
+            })
             })
         })
+    }
 
+    renderWork();
+
+//-----------------------------end of work data-----------------------------------
+//----------------------start of intro data---------------------------------
+
+
+    let introContent = document.querySelector("#introContent")
+    function renderIntro(){
+
+        document.getElementById("introContent").innerHTML ='';
+    db.collection('introductions').get().then(snapshot => {
+        snapshot.docs.forEach(doc => {
+            // renderIntro(doc);
+
+            let intro_div = document.createElement('div');
+            intro_div.classList.add("introData");
+            intro_div.setAttribute('id', doc.id);
+        
+            let intro = document.createElement('div');
+                intro.classList.add("intro");
+                intro.textContent = doc.data().fact;
+                intro_div.appendChild(intro);
+                introContent.appendChild(intro_div);
+ 
+                let refresh = document.createElement('div');
+                refresh.textContent= '';
+                refresh.addEventListener('click', (event) => {
+                    event.stopPropagation();
+                    let getid = event.target.parentElement.getAttribute('id');
+                    db.collection('introductions').doc(getid).delete();
+                    renderIntro();
+                })
+        })
+    })
+}
+    //getting data netninja
+    renderIntro();
 
 
 //------------------------------start of links data-----------------------------
@@ -264,7 +257,6 @@ function renderLink(doc){
         link_div.appendChild(link);
       
         linkContent.appendChild(link_div);
-
 
 }
 //getting data netninja
@@ -307,7 +299,8 @@ function renderLink(doc){
             year_start: addWorkForm.workAddstart.value,
             year_end: addWorkForm.workAddend.value
         }).then(function(doc){
-            console.log('item added w ID: ' + doc.id)
+            console.log('item added w ID: ' + doc.id);
+            renderWork();
         })
     })
 
@@ -323,11 +316,10 @@ function renderLink(doc){
             year_start: addOrgForm.orgAddstart.value,
             year_end: addOrgForm.orgAddend.value
         }).then(function(doc){
-            console.log('item added w ID: ' + doc.id)
+            console.log('item added w ID: ' + doc.id);
+            renderOrg();
         })
     })
-
-
 
     // EDIT DATA
 
@@ -335,25 +327,66 @@ function renderLink(doc){
     // //whenever user presses the button, this will happen
     editIntroForm.addEventListener('submit', (event) =>{
         event.preventDefault();
-        db.collection('introductions').add({
+        db.collection('introductions').doc('desc').update({
             fact: editIntroForm.introEditfact.value,
-        }).then(function(doc){
-            console.log(doc.id + " edited!")
+        }).then(function(){
+            console.log("oks na");
+            renderIntro();
         })
     })
 
 
 
-    const addLinkForm = document.querySelector('#editLinkForm');
+
+    const editLinkFormTwitter = document.querySelector('#editLinkFormTwitter');
+    const editLinkFormGithub = document.querySelector('#editLinkFormGithub');
+    const editLinkFormLinkedin = document.querySelector('#editLinkFormLinkedin');
     // //whenever user presses the button, this will happen
-    addLinkForm.addEventListener('button', (event) =>{
-        event.reloadPage(true);
-        db.collection('links').update({
-            link: addLinkForm.linkEdit.value,
+
+    
+    editLinkFormTwitter.addEventListener('button', (event) =>{
+        event.preventDefault();
+
+        db.collection('links').doc('twitter').update({
+            twitter: editLinkFormTwitter.linkEditTwitter.value,
         }).then(function(doc){
             console.log('item added w ID: ' + doc.id)
         })
     })
+
+    editLinkFormGithub.addEventListener('button', (event) =>{
+        event.preventDefault();
+
+        db.collection('links').doc('github').update({
+            github: editLinkFormGithub.linkEditGithub.value,
+        }).then(function(doc){
+            console.log('item added w ID: ' + doc.id)
+        })
+    })
+
+    editLinkFormLinkedin.addEventListener('button', (event) =>{
+        event.preventDefault();
+
+        db.collection('links').doc('linkedin').update({
+            linkedin: editLinkFormLinkedin.linkEditLinkedin.value,
+        }).then(function(doc){
+            console.log('item added w ID: ' + doc.id)
+        })
+    })
+
+
+    function goGithub() {
+        window.open("https://camillecay.github.io/MP1Project/", "_blank");
+    }
+    
+    function goTwitter() {
+        window.open("https://twitter.com/camille_cay", "_blank");
+    }
+    
+    function goLinkedin() {
+        window.open("www.linkedin.com/in/camille-cay-060500", "_blank");
+    }
+    
 
 // COLLAPSE BUTTON STUFF
 function educopenNav() {
@@ -403,3 +436,5 @@ function linkcloseNav() {
     document.getElementById("linksideBar").style.width = "0";
   }
   
+
+
